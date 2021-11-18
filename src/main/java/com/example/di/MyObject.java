cccd36943cc11ba2e94804f41ee24d3c2a389a40
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-11-18 19:40:46 +0900

    마지막 생성자 bean 생성

--- src/main/java/com/example/di/MyObject.java
+++ src/main/java/com/example/di/MyObject.java
@@ -2,10 +2,37 @@ package com.example.di;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 public class MyObject {
+	
+	private String aaa;
+	private Member objMember;
+	private Properties props;
+	
 	List<String> list;
 	int[] intArr;
+
+	
+	
+	
+	public void setAaa(String aaa) {
+		this.aaa = aaa;
+	}
+
+	public void setObjMember(Member objMember) {
+		this.objMember = objMember;
+	}
+
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+
+	public MyObject(String aaa, Member objMember) {
+		super();
+		this.aaa = aaa;
+		this.objMember = objMember;
+	}
 
 	public void setIntArr(int[] intArr) {
 		this.intArr = intArr;
